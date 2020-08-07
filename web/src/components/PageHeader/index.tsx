645a8f0abd1352ba504a3d287636d2f81be1cdde
jeandobre <jeandobre@ufgd.edu.br>
2020-08-07 19:34:30 -0400

    Criado projeto fron end

---
+++ web/src/components/PageHeader/index.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import logoImg from "../../assets/images/logo.svg";
+import backIcon from "../../assets/images/icons/back.svg";
+
+import "./styles.css";
+
+interface PageHeaderProps {
+	title: string;
+	description?: String; /* propriedade não obrigatória */
+}
+
+const PageHeader: React.FC<PageHeaderProps> = (props) => {
+	return (
+		<div id="page-teacher-list" className="container">
+			<header className="page-header">
+				<div className="top-bar-container">
+					<Link to="/">
+						<img src={backIcon} alt="Voltar"/>
+					</Link>
+					<img src={logoImg} alt="Proffy"/>
+				</div>
+
+				<div className="header-content">
+					<strong>{props.title}</strong>
+					{ props.description && <p>{ props.description }</p> }
+
+					{ props.children }
+				</div>			
+			</header>			
+		</div>
+	)
+}
+
+export default PageHeader;
